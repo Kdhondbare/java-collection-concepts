53ca943d037a7a87e344865bc0b354b2b30cec88
Karan Dhondbare <kdhondbare123@gmail.com>
2023-08-27 23:35:17 +0530

    Adding Queue example.

--- queue-example/src/QueueJavaExample.java
+++ queue-example/src/QueueJavaExample.java
@@ -5,5 +5,13 @@ public class QueueJavaExample {
 
     public static void main(String[] args) {
         Queue<Integer> integerQueue = new LinkedList<>();
+
+        integerQueue.add(11);
+        integerQueue.add(21);
+        integerQueue.add(31);
+        integerQueue.remove();
+
+        System.out.println("Front item : " + integerQueue.peek());
+
     }
 }
