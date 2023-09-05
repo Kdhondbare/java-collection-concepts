6db41cfeeaba7bf44b5a1d3366c84c687f5e3876
Karan Dhondbare <kdhondbare123@gmail.com>
2023-09-05 23:45:38 +0530

    Adding Java linked list Example.

---
+++ list-example/src/JavaLinkedListExample.java
@@ -0,0 +1,50 @@
+/*
+Iterable
+  ↓
+Collection
+  ↓
+List
+  ↓
+LinkedList
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class JavaLinkedListExample {
+
+    public static void main(String[] args) {
+
+        List<String> guestList = new LinkedList<>();
+
+        guestList.add("Karan");
+        guestList.add("Aarti");
+        guestList.add("Komal");
+        guestList.add("Vikram");
+
+        for (String name : guestList) {
+            System.out.println(name);
+        }
+
+        guestList.forEach(System.out::println);
+        System.out.println("------------------");
+        guestList.add(0, "Bhagyashree");
+
+        guestList.forEach(System.out::println);
+        System.out.println("------------------");
+        guestList.add(2,"Pratik");
+
+        guestList.forEach(System.out::println);
+        System.out.println("------------------");
+        guestList.remove(0);
+
+        guestList.forEach(System.out::println);
+        System.out.println("------------------");
+
+        guestList.remove(2);
+
+        guestList.forEach(System.out::println);
+        System.out.println("------------------");
+
+    }
+}
