389f03cce03ca9a1b50c0396b0898dc5a0fa3750
Karan Dhondbare <kdhondbare123@gmail.com>
2023-09-14 23:41:10 +0530

    Adding Java Array list Example.

---
+++ list-example/src/JavaArrayListExample.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Iterable
+↓
+Collection
+↓
+List
+↓
+ArrayList
+
+ */
+public class JavaArrayListExample {
+
+    public static void main(String[] args) {
+
+        List<String> guestList = new ArrayList<>(2);
+
+        //base address + index*size of address stored.
+        guestList.add("Karan");
+        guestList.add("Aarti");
+        guestList.add("Komal");
+        guestList.add("Vikram");
+
+        guestList.forEach(System.out::println);
+        System.out.println("------------------");
+        guestList.add(0, "Bhagyashsree");
+
+        guestList.forEach(System.out::println);
+        System.out.println("-------------------");
+        guestList.add(2, "Pratik");
+
+        guestList.forEach(System.out::println);
+        System.out.println("--------------------");
+        guestList.remove(0);
+
+        guestList.forEach(System.out::println);
+        System.out.println("---------------------");
+
+        guestList.remove(2);
+
+        guestList.forEach(System.out::println);
+        System.out.println("----------------------");
+    }
+}
